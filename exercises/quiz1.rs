ebe5017560380c8e87d25cb7728ee6c83f2ba60e
Conor Brady <con.brad@gmail.com>
2024-03-03 09:36:53 -0800

    Remove not

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,7 +13,7 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+// I AM DONE
 
 fn calculate_price_of_apples(num_apples: i32) -> i32{
     if (num_apples > 40) {num_apples} else {num_apples * 2}
